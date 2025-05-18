cb797ee4e513177b7a5d7cd7f4812d999fa30547
CodyNotFound <codynotfound@outlook.com>
2025-05-18 09:33:09 +0800

    feat: add Category model and categories endpoint

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,11 +3,22 @@ import type { Context } from 'hono';
 import { Controller } from '@/decorators/controller';
 import { Get } from '@/decorators/http';
 import { ResponseUtil } from '@/core/response';
+import { prisma } from '@/utils/db';
 
 @Controller('/')
-export class AdminController {
+export class IndexController {
     @Get('/')
     async index(c: Context) {
         return ResponseUtil.success(c, 'Hello World');
+    }
+
+    @Get('/categories')
+    async categories(c: Context) {
+        const categories = await prisma.category.findMany({
+            orderBy: {
+                index: 'asc',
+            },
+        });
+        return ResponseUtil.success(c, categories);
     }
 }
