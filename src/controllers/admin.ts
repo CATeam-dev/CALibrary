7c9e6e1c7b0f5350d6b4bb763abacfd333af1558
CodyNotFound <codynotfound@outlook.com>
2025-05-23 11:13:12 +0800

    feat: admin auth

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -54,7 +54,7 @@ export class AdminController {
             })
         );
 
-        return ResponseUtil.success(c, { categories: categoriesWithBookCount });
+        return ResponseUtil.success(c, categoriesWithBookCount);
     }
 
     @Get('/category/:path', telegramAuthMiddleware)
